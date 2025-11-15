ceaf39928a7c9c08002638e1bf006b6ca45b39ca
seletivoacqua <seletivoinstitutoacqua@gmail.com>
2025-11-14 22:21:43 -0300

    Update userService.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -59,7 +59,7 @@ class GoogleSheetsService {
   }
 }
 
-const SCRIPT_URL = import.meta.env.VITE_GOOGLE_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbwRZ7vLEm4n8iha2GJSnIfCEjhHejRLme-OkIkp_qu6/dev';
+const SCRIPT_URL = import.meta.env.VITE_GOOGLE_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbwQK8whrNC039L5V-7JeVLAjkdsrPM65L3YhdCPPWrF782uQv5UTdsitYMwUS9BQuyj/exec';
 const sheetsService = new GoogleSheetsService(SCRIPT_URL);
 
 export async function getUsers(): Promise<User[]> {
